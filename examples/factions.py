cf3d6cc9a4ddc33f04912ae2d10c7b6e1cbf2ced
Oskar Sailer <oskar.sailer@sap.com>
2020-11-04 12:38:26 +0100

    ignore mac related stuff

--- examples/factions.py
+++ examples/factions.py
@@ -1,21 +1,21 @@
-factions = {}
+fractions = {}
 
-factions['SPD'] = ['SPD', 'Sozialdemokratische Partei', 'Sozialdemokraten', 'Sozialdemokrat', 'Rot']
-factions['CDU'] = ['CDU', 'Christlich Demokratische Union', 'Christlich Demokratische Union', 'Union', 'Schwarz']
-factions['CSU'] = ['CSU', 'Christlich-Soziale Union', 'Christlich-Soziale Union', 'Union']
-factions['Bündnis 90/Die Grünen'] = ['Grün', 'Bündnis 90']
-factions['Die Linke'] = ['Die Linke', 'Linke' 'Linkspartei', 'Rot']
-factions['AfD'] = ['AFD', 'Alternative für Deutschland', 'Blau']
-factions['FDP'] = ['Freie Demokratische Partei', 'Freie Demokraten', 'Liberale', 'Gelb']
+fractions['SPD'] = ['SPD', 'Sozialdemokratische Partei', 'Sozialdemokraten', 'Sozialdemokrat', 'Rot']
+fractions['CDU'] = ['CDU', 'Christlich Demokratische Union', 'Christlich Demokratische Union', 'Union', 'Schwarz']
+fractions['CSU'] = ['CSU', 'Christlich-Soziale Union', 'Christlich-Soziale Union', 'Union']
+fractions['Bündnis 90/Die Grünen'] = ['Grün', 'Bündnis 90']
+fractions['Die Linke'] = ['Die Linke', 'Linke' 'Linkspartei', 'Rot']
+fractions['AfD'] = ['AFD', 'Alternative für Deutschland', 'Blau']
+fractions['FDP'] = ['Freie Demokratische Partei', 'Freie Demokraten', 'Liberale', 'Gelb']
 
-factions_historical = factions
+fractions_historical = fractions
 
-factions_historical['GB/BHE'] = ['Gesamtdeutscher Block/Bund der Heimatvertriebenen und Entrechteten', 'BHE']
-factions_historical['BP'] = ['BP', 'Bayernpartei']
-factions_historical['DP'] = ['DP', 'Deutsche Partei']
-factions_historical['NDP'] = ['NDP', 'Nationaldemokratische Partei', 'National-Demokratische Partei']
-factions_historical['KPD'] = ['KPD', 'Kommunistische Partei']
-factions_historical['WAV'] = ['WAV', 'Wirtschaftliche Aufbau-Vereinigung']
-factions_historical['DZP'] = ['DZP', 'Deutsche Zentrumspartei', 'Z']
-factions_historical['DRP'] = ['DRP', 'Deutsche Reichs-Partei', 'Deutsche Rechtspartei']
-factions_historical['DKP'] = ['DKP', 'Deutsche Konservative Partei']
+fractions_historical['GB/BHE'] = ['Gesamtdeutscher Block/Bund der Heimatvertriebenen und Entrechteten', 'BHE']
+fractions_historical['BP'] = ['BP', 'Bayernpartei']
+fractions_historical['DP'] = ['DP', 'Deutsche Partei']
+fractions_historical['NDP'] = ['NDP', 'Nationaldemokratische Partei', 'National-Demokratische Partei']
+fractions_historical['KPD'] = ['KPD', 'Kommunistische Partei']
+fractions_historical['WAV'] = ['WAV', 'Wirtschaftliche Aufbau-Vereinigung']
+fractions_historical['DZP'] = ['DZP', 'Deutsche Zentrumspartei', 'Z']
+fractions_historical['DRP'] = ['DRP', 'Deutsche Reichs-Partei', 'Deutsche Rechtspartei']
+fractions_historical['DKP'] = ['DKP', 'Deutsche Konservative Partei']
