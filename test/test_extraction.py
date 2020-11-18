5d071c37c0410af0629acadc7f05b057408a996f
menno4000 <maxluedemann@t-online.de>
2020-11-18 13:59:44 +0100

    new test cases #2

--- test/test_extraction.py
+++ test/test_extraction.py
@@ -76,3 +76,58 @@ class TestExtraction(unittest.TestCase):
         self.assertEqual(interaction_1.sender, Faction.DIE_GRÜNEN)
         self.assertEqual(interaction_1.message, 'Zuruf vom BÜNDNIS90/DIE GRÜNEN: Vielleicht mal lesen! Lesen bildet!')
 
+
+    def test_extract_funny_sample1(self):
+        comment = "(Heiterkeit und Beifall bei Abgeordneten der CDU/CSU, der SPD und der LINKEN – Lachen bei der AfD)"
+
+        cm = extract_communication_model([_build_candidate(comment)])
+        interaction_0 = cm[0]
+        interaction_1 = cm[1]
+        interaction_2 = cm[2]
+        interaction_3 = cm[3]
+
+        self.assertEqual(interaction_0.sender, Faction.CDU_AND_CSU)
+        self.assertEqual(interaction_0.message, 'Heiterkeit und Beifall bei Abgeordneten der CDU/CSU, der SPD und der LINKEN')
+        self.assertEqual(interaction_1.sender, Faction.SPD)
+        self.assertEqual(interaction_1.message, 'Heiterkeit und Beifall bei Abgeordneten der CDU/CSU, der SPD und der LINKEN')
+        self.assertEqual(interaction_2.sender, Faction.DIE_LINKE)
+        self.assertEqual(interaction_2.message, 'Heiterkeit und Beifall bei Abgeordneten der CDU/CSU, der SPD und der LINKEN')
+        self.assertEqual(interaction_3.sender, Faction.AFD)
+        self.assertEqual(interaction_3.message, 'Lachen bei der AfD')
+
+    def test_extract_funny_sample2(self):
+        comment = "(Beifall bei der SPD sowie bei Abgeordneten der LINKEN – Matthias W. Birkwald [DIE LINKE]: Ich mich auch!)"
+
+        cm = extract_communication_model([_build_candidate(comment)])
+        interaction_0 = cm[0]
+        interaction_1 = cm[1]
+        interaction_2 = cm[2]
+
+        self.assertEqual(interaction_0.sender, Faction.SPD)
+        self.assertEqual(interaction_0.message, 'Beifall bei der SPD sowie bei Abgeordneten der LINKEN')
+        self.assertEqual(interaction_1.sender, Faction.DIE_LINKE)
+        self.assertEqual(interaction_1.message, 'Beifall bei der SPD sowie bei Abgeordneten der LINKEN')
+        self.assertEqual(interaction_2.sender, MDB.find_in_storage(forename="Matthias W.", surname="Birkwald", memberships=[(datetime.min, None, Faction.DIE_LINKE)]))
+        self.assertEqual(interaction_2.message, 'Ich mich auch!')
+
+    def test_extract_funny_sample_3(self):
+        comment = "(Heiterkeit und Beifall bei der CDU/CSU sowie des Abg. Jens Beeck [FDP])"
+
+        cm = extract_communication_model([_build_candidate(comment)])
+        interaction_0 = cm[0]
+        interaction_1 = cm[1]
+
+        self.assertEqual(interaction_0.sender, Faction.CDU_AND_CSU)
+        self.assertEqual(interaction_0.message, 'Heiterkeit und Beifall bei der CDU/CSU sowie des Abg. Jens Beeck [FDP]')
+        self.assertEqual(interaction_1.sender, MDB.find_in_storage(forename="Jens", surname="Beeck", memberships=[(datetime.min, None, Faction.FDP)]))
+        self.assertEqual(interaction_1.message, 'Heiterkeit und Beifall bei der CDU/CSU sowie des Abg. Jens Beeck [FDP]')
+
+    def test_extract_funny_sample_4(self):
+        comment = "(Heiterkeit des Abg. Manfred Grund [CDU/CSU])"
+
+        cm = extract_communication_model([_build_candidate(comment)])
+        interaction_0 = cm[0]
+
+        self.assertEqual(interaction_0.sender, MDB.find_in_storage(forename="Manfred", surname="Grund", memberships=[(datetime.min, None, Faction.CDU_AND_CSU)]))
+        self.assertEqual(interaction_0.message, 'Heiterkeit des Abg. Manfred Grund [CDU/CSU]')
+
