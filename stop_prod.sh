ee88934e221feed1c6d31e1d770414f671efb0f0
Youri Seichter <yseichter@gmail.com>
2021-01-08 13:48:49 +0100

    Add stop_prod script

---
+++ stop_prod.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $SCRIPT_DIR
+
+echo "Stopping services"
+echo "----------------------------------------"
+docker-compose -f ./docker-compose.prod.yml --env-file ./.env.prod stop
+echo ""
+
