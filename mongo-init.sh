90ae64365f67b8ea5c99a59e33bb8caa41f4f0bf
menno4000 <maxluedemann@t-online.de>
2021-01-15 12:23:24 +0100

    unwreck the mongo init

--- mongo-init.sh
+++ mongo-init.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-echo hello
-#set -e
-#
-#mongo <<EOF
-#use cme_data
-#db.createUser({
-#  user:  '$CME_DB_USERNAME',
-#  pwd: '$CME_DB_PASSWORD',
-#  roles: [{
-#    role: 'readWrite',
-#    db: '$CME_DB_NAME'
-#  }]
-#})
-#EOF
+set -e
+
+mongo <<EOF
+use cme_data
+db.createUser({
+  user:  '$CME_DB_USERNAME',
+  pwd: '$CME_DB_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$CME_DB_NAME'
+  }]
+})
+EOF
