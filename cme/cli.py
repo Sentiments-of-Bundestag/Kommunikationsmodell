6c57840ae08c32cd379ac98c12008774ec102993
Youri Seichter <yseichter@gmail.com>
2021-01-17 13:38:58 +0100

    improve help in argparse cli

--- cme/cli.py
+++ cme/cli.py
@@ -143,31 +143,52 @@ def main():
 
     subparsers = parser.add_subparsers()
 
-    manual_parser = subparsers.add_parser("manual", aliases=["m"])
-    manual_parser.add_argument("files", nargs="+", type=Path)
-    manual_parser.add_argument("--dry-run", default=False, action="store_true")
-    manual_parser.add_argument("--add-debug-objects", default=False, action="store_true")
-    manual_parser.add_argument("--notify", default=False, action="store_true")
+    manual_parser = subparsers.add_parser("manual", aliases=["m"],
+                                          help='Manually read files for communication extraction.')
+    manual_parser.add_argument("files", nargs="+", type=Path,
+                               help="Path to one or more .json (from Crawler service) or .xml (from official "
+                                    "Bundestag protocols) files. Must contain protocols of Bundestag.")
+    manual_parser.add_argument("--dry-run", default=False, action="store_true",
+                               help="Set to true if you do not want to save data into the database. (Default: False)")
+    manual_parser.add_argument("--add-debug-objects", default=False, action="store_true",
+                               help="While assembling communication data, save debug information (eg where the data "
+                                    "comes from) into the communication objects. (Default: False)")
+    manual_parser.add_argument("--notify", default=False, action="store_true",
+                               help="Notify Group 3/Sentiment Analyses via HTTP request about new protocols. "
+                                    "(Default: False)")
     manual_parser.set_defaults(func=manual_mode)
 
-    dump_parser = subparsers.add_parser("dump", aliases=["d"])
-    dump_parser.add_argument("--database", type=str, default="cme", choices=["cme", "crawler"])
-    dump_parser.add_argument("--index-field", type=str, default="_id")
-    dump_parser.add_argument("--index", type=str, default="")
-    dump_parser.add_argument("--list-collections", default=False, action="store_true")
-    dump_parser.add_argument("--list-collection-fields", default=False, action="store_true")
-    dump_parser.add_argument("--collection", type=str)
-    dump_parser.add_argument("--output-file", type=Path)
+    dump_parser = subparsers.add_parser("dump", aliases=["d"], help="Let's you extract database raw data. "
+                                                                    "Useful for debugging.")
+    dump_parser.add_argument("--database", type=str, default="cme", choices=["cme", "crawler"],
+                             help="Specify the Database you want to dump data from (Default: cme)")
+    dump_parser.add_argument("--index-field", type=str, default="_id",
+                             help="Specify the index field (???). (Default: _id)")
+    dump_parser.add_argument("--index", type=str, default="",
+                             help=" Specify the index. (Default: id)")
+    dump_parser.add_argument("--list-collections", default=False, action="store_true",
+                             help="Set to true if you want to list all available collections. "
+                                  "Needs the --collection option. (Default: False)")
+    dump_parser.add_argument("--list-collection-fields", default=False, action="store_true",
+                             help="Set to true if you want to list all collection fields. "
+                                  "Needs the --collection option. (Default: False)")
+    dump_parser.add_argument("--collection", type=str,
+                             help="Specify the collection you want to operate on.")
+    dump_parser.add_argument("--output-file", type=Path,
+                             help="Specify the output file of the dump.")
     dump_parser.set_defaults(func=dump_mode)
 
-    server_parser = subparsers.add_parser("server", aliases=["s"])
-    server_parser.add_argument("--host", default="127.0.0.1", type=str)
-    server_parser.add_argument("--port", default="9001", type=int)
-    server_parser.add_argument("--reload", default=False, action="store_true")
+    server_parser = subparsers.add_parser("server", aliases=["s"],
+                                          help="Start the server. Includes the REST API as well as documentation.")
+    server_parser.add_argument("--host", default="127.0.0.1", type=str, help="Define the host. (Default: 127.0.0.1)")
+    server_parser.add_argument("--port", default="9001", type=int, help="Define the port. (Default: 9001)")
+    server_parser.add_argument("--reload", default=False, action="store_true",
+                               help="Set to true for reload on file change (Default: False)")
     server_parser.set_defaults(func=server_mode)
 
     init_parser = subparsers.add_parser("init", aliases=["i"],
-                                        help="Generates a new mdb collection locally from the crawler db (default) or file (see --file)")
+                                        help="Generates a new mdb collection locally from the crawler db (default) or"
+                                             " file (see --file)")
     init_parser.add_argument("--file", type=Path, help="Path of a json you want to use instead of the remote crawler")
     init_parser.set_defaults(func=initialize)
 
